61bb0806367d2d8e3c59e5920f5aa774939f7850
Florian Schaller <florian.schaller@heig-vd.ch>
2018-06-28 20:38:16 +0200

    Add logging for debugging

--- api/routes/api.campaign.js
+++ api/routes/api.campaign.js
@@ -761,6 +761,7 @@ campaign.compileJSONIntoYaml = (req, res, next) => {
 campaign.buildCampaign = (req, res, next) => {
 
     console.log('Build in progress...');
+    console.log(path.normalize(__dirname + '../../../emails-engine'));
     const build = spawn('npm run', ['build', `${req.params.brandSlug}/${req.params.campaignSlug}`], {
         cwd: path.normalize(__dirname + '../../../emails-engine')
     });
