5dea5683df061c4c56c23b6e0b70309a2ea66a3c
Florian Schaller <florian.schaller@heig-vd.ch>
2018-06-28 16:25:01 +0200

    Change localhost to heroku

--- ui/src/app/config.ts
+++ ui/src/app/config.ts
@@ -1,3 +1,9 @@
-export const config = {
+// Local test config
+/*export const config = {
     apiBaseUrl: 'http://localhost:3000/api',
+};*/
+
+// Config for Heroku Deployment
+export const config = {
+    apiBaseUrl: 'https://emails-engine-app.herokuapp.com/api',
 };
